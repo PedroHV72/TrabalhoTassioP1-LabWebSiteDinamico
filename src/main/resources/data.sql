b53e522e0bdee740f1b4ea4aa8426bcf7940e193
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2021-09-27 19:46:22 -0300

    Criacao das tabelas e cadastros das classes Usuario, Cliente, Fornecedora e Produto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,15 @@ VALUES (1, 'Alloy FPS Pro', 'Teclado mecanico', '', '2021-09-27', '429.90');
 
 INSERT INTO produto(id, nome, descricao, fotoUrl, dataCadastro, valorUnitario)
 VALUES (2, 'G403 Hero 16K', 'Mouse gamer', '', '2021-09-27', '269.90');
+
+INSERT INTO usuario(id, nome, email, senha)
+VALUES (1, 'Pedro', 'a@a.com', '123');
+
+INSERT INTO usuario(id, nome, email, senha)
+VALUES (2, 'Kabum', 'b@b.com', '456');
+
+INSERT INTO cliente(cpf, usuarioId)
+VALUES ('72', 1);
+
+INSERT INTO fornecedora(cnpj, usuarioId)
+VALUES ('99', 2)
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,25 @@
 CREATE TABLE produto (
-     id                        INT           NOT NULL,
-     nome                      VARCHAR(40)   NOT NULL,
-     descricao                 VARCHAR(300),
-     fotoUrl                   VARCHAR(1000),
-     dataCadastro              DATE,
-     valorUnitario             FLOAT(5.2)
+    id                        INT           NOT NULL,
+    nome                      VARCHAR(40)   NOT NULL,
+    descricao                 VARCHAR(300),
+    fotoUrl                   VARCHAR(1000),
+    dataCadastro              DATE,
+    valorUnitario             FLOAT(5.2)
+);
+
+CREATE TABLE usuario (
+    id    INT         NOT NULL,
+    nome  VARCHAR(40) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    senha VARCHAR(16) NOT NULL
+);
+
+CREATE TABLE cliente (
+    cpf VARCHAR(11) NOT NULL,
+    FOREIGN KEY (usuarioId) REFERENCES usuario(id),
+);
+
+CREATE TABLE fornecedora (
+    cnpj VARCHAR(14) NOT NULL,
+    FOREIGN KEY (usuarioId) REFERENCES usuario(id),
 );
