f77e4a5447fbc76ad3207f0659411065a692c8f6
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2021-11-15 16:20:11 -0300

    Mexendo nos cadastros

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,17 @@
 INSERT INTO produto(id, nome, descricao, fotoUrl, dataCadastro, valorUnitario)
-VALUES (1, 'Alloy FPS Pro', 'Teclado mecanico', '', '2021-09-27', '429.90');
+VALUES (1, 'Hórus', 'Pré treino', '', '2021-09-27', '59.90');
 
 INSERT INTO produto(id, nome, descricao, fotoUrl, dataCadastro, valorUnitario)
-VALUES (2, 'G403 Hero 16K', 'Mouse gamer', '', '2021-09-27', '269.90');
+VALUES (2, 'Whey', '100% Proteína', '', '2021-09-27', '109.90');
+
+INSERT INTO produto(id, nome, descricao, fotoUrl, dataCadastro, valorUnitario)
+VALUES (3, 'Barra Cereal', 'Totalmente proteica', '', '2021-09-27', '4.90');
 
 INSERT INTO usuario(id, nome, email, senha)
 VALUES (1, 'Pedro', 'a@a.com', '123');
 
 INSERT INTO usuario(id, nome, email, senha)
-VALUES (2, 'Kabum', 'b@b.com', '456');
+VALUES (2, 'Max Titanium', 'b@b.com', '456');
 
 INSERT INTO cliente(cpf, usuarioId)
 VALUES ('72', 1);
